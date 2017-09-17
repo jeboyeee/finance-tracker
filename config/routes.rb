bd6c083c0a38a5a4e4af6421e196acd4b4ce124a
Rails udemy Course <jeboyzz@yahoo.com>
2017-09-17 13:28:43 +0000

    add frindship model

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
+  get 'my_friends', to: "users#my_friends"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
