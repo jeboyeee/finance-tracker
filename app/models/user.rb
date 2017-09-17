75d6e3f2d23c3f117c549c5da931c421a1e2aff0
Rails udemy Course <jeboyzz@yahoo.com>
2017-09-17 15:27:11 +0000

    Add friendship and search functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,38 @@ class User < ActiveRecord::Base
     "Anonymous"
   end
   
+  def not_friends_with?(friend_id)
+    friendships.where(friend_id: friend_id).count < 1
+  end
+  
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
+  end
+  
+  # in users_controller.rb = def search @users = User.search(params[:search_params])
+  def self.search(param)
+    return User.none if param.blank?
+    
+    param.strip!
+    param.downcase!
+    (first_name_matches(param) + last_name_macthes(param) + email_matches(param)).uniq
+  end
+  
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+  
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+  
+  def self.email_matches(param)
+    matches('email', param)    
+  end
+  
+  def self.matches(field_name, param)
+    # % wild card - doent have tobe exact match as long as the search string as somethin that was found in the field
+    where("lower(#{field_name}) like ?", "%#{param}%")
+  end
+  
 end
